fea441bf3f3d66223e4b070c32ea78d024a3192d
Legionas07 <2022500070@mahasiswa.atmaluhur.ac.id>
2024-09-02 15:24:06 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque expedita dolorum quia ex, culpa eos obcaecati perspiciatis autem mollitia asperiores voluptate, ratione quos, sint molestias tempora ab libero officiis nisi?</p>
+<ul>
+    <li>Satu</li>
+    <li>dua</li>
+</ul>
 </body>
 </html>
